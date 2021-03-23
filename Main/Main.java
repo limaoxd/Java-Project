e8297e3aa8bccf152dc45671695c34c823aeff9e
root <root@MSI.localdomain>
2021-03-24 00:42:59 +0800

    First commit, we can do everything here~

---
+++ Main/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Class{
+    public static void main(String[] args){
+        
+        Class hi = new Class();
+        System.out.println(hi.port);
+        System.out.println(Class.port);
+        
+    }
+}
