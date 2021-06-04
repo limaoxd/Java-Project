bd5d9511aa22e9633b2bb2db1cb61178b57bbf2b
limaox <limaoxgood@gmail.com>
2021-06-04 11:45:38 +0800

    Add spike

--- Main/entity/Book.java
+++ Main/entity/Book.java
@@ -17,8 +17,11 @@ import javafx.scene.shape.Rectangle;
 
 public class Book extends Entity{
 
-    public Book(int w,int h,double x,double y) throws FileNotFoundException{
-        image=new Image(new FileInputStream("pic/p1.png"));
+    public Book(int w,int h,double x,double y,int type) throws FileNotFoundException{
+        
+        if(type==1) image=new Image(new FileInputStream("pic/p1.png"));
+        else if(type==2) image=extendSprite(new Image(new FileInputStream("pic/spike_d.png")),w);
+        else if(type==3) image=extendSprite(new Image(new FileInputStream("pic/spike_u.png")),w);
         sprite = new ImageView(image);
         setSize(w,h);
         setPos(x,y);
@@ -26,16 +29,61 @@ public class Book extends Entity{
 
     public static void createBook(List<Book> book) throws FileNotFoundException{
         int i=0;
+        double length_2=0;
+        double length_3=0;
         for(String col : MAP.book1){
             for(int j = 0;j<col.length();j++){
-                if(col.charAt(j)<='9'&&col.charAt(j)>'0'){
+                if(col.charAt(j)>'0'&&col.charAt(j)<='9'){
                     int height = (int)col.charAt(j)-'0';
-                    Book pillier = new Book(200,150*height,200*(j+0.5)+50,150*(MAP.book1.length-1-i));
+                    Book pillier = new Book(200,150*height,200*(j+0.5)+50,150*(MAP.book1.length-1-i),1);
                     book.add(pillier);
+                }
+                if(col.charAt(j)=='d'){
+                    length_2++;
+                    //If col end but number is "2"
+                    if(j==col.length()-1){
+                        Book spike = new Book((int)length_2*200,150,200*(j+1-length_2/2)+50,150*(MAP.map1.length-1-i),2);
+                        book.add(spike);
+                    }
+                }
+                else if(length_2>0){
+                    Book spike = new Book((int)length_2*200,150,200*(j-length_2/2)+50,150*(MAP.map1.length-1-i),2);
+                    book.add(spike);
+                    length_2 = 0;
+                }
+                if(col.charAt(j)=='u'){
+                    length_3++;
+                    //If col end but number is "2"
+                    if(j==col.length()-1){
+                        Book spike = new Book((int)length_3*200,150,200*(j+1-length_3/2)+50,150*(MAP.map1.length-1-i),3);
+                        book.add(spike);
+                    }
+                }
+                else if(length_3>0){
+                    Book spike = new Book((int)length_3*200,150,200*(j-length_3/2)+50,150*(MAP.map1.length-1-i),3);
+                    book.add(spike);
+                    length_3 = 0;
                 }
             }
             i++;
          }
+    }
+
+    public Image extendSprite(Image img,int w){
+        int width = 200,height = 150;
+        int blocklength=w/width;
+
+        PixelReader pixelReader = img.getPixelReader();
+
+        WritableImage c_img = new WritableImage(w,height);
+        PixelWriter writer = c_img.getPixelWriter();
+        for(int i=0; i<height; i++){
+            for(int j=0; j<w; j++){
+                Color color = pixelReader.getColor(j%width,i);
+                writer.setColor(j,i,color);
+            }
+        }
+        return (Image)c_img;
     }
 
     @Override
